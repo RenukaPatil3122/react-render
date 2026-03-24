49470c42e92abab19d2e1439c63ffe7d8fff16a4
Renuka Patil <renukapatil627@gmail.com>
2026-03-24 17:28:43 +0530

    useReducer rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { UseState } from "./components/UseState/UseState";
+import { UseReducer } from "./components/UseReducer/UseReducer";
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      <UseReducer />
+
+      {/* <UseState /> */}
     </div>
   );
 }
