eacfd846c9a5ef50984fef0bee898a5cb3dfe0c4
Renuka Patil <renukapatil627@gmail.com>
2026-03-25 12:10:27 +0530

    Parent Child re render

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { UseState } from "./components/UseState/UseState";
 import { UseReducer } from "./components/UseReducer/UseReducer";
 import { ObjectUseState } from "./components/Immutable State/ObjectUseState";
 import { ArrayUseState } from "./components/Immutable State/ArrayUseState";
+import { Parent } from "./components/Parent Child/Parent";
 
 function App() {
   return (
     <div className="App">
-      <ArrayUseState />
+      <Parent />
+
+      {/* <ArrayUseState /> */}
 
       {/* <ObjectUseState /> */}
 
